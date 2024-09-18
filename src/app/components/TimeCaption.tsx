f3171c4a155cd4c0d3f05e11c6cc5ee140f5e280
tommy10092525 <tommyxla777@gmail.com>
2024-09-19 06:53:49 +0900

    時刻表示の間違いの修正

--- src/app/components/TimeCaption.tsx
+++ src/app/components/TimeCaption.tsx
@@ -22,18 +22,18 @@ const TimeCaption = (props: TimeCaptionProps) => {
         </div>
         <div className="flex justify-center mx-0 text-center text-4xl font-bold">
           <p className="px-1">{!isLoading && firstBus ? minutesToTime(firstBus.leaveHour * 60 + firstBus.leaveMinute) : "loading"}</p>
-          <p className="px-1">{!isLoading && firstBus ? minutesToTime(firstBus.arriveHour * 60 + firstBus.leaveMinute) : "loading"}</p>
+          <p className="px-1">{!isLoading && firstBus ? minutesToTime(firstBus.arriveHour * 60 + firstBus.arriveMinute) : "loading"}</p>
         </div>
         <div className="flex justify-center mx-0 text-center text-2xl font-bold opacity-50">
           <p className="px-1">{!isLoading && secondBus ? minutesToTime(secondBus.leaveHour * 60 + secondBus.leaveMinute) : "loading"}</p>
-          <p className="px-1">{!isLoading && secondBus ? minutesToTime(secondBus.arriveHour * 60 + secondBus.leaveMinute) : "loading"}</p>
+          <p className="px-1">{!isLoading && secondBus ? minutesToTime(secondBus.arriveHour * 60 + secondBus.arriveMinute) : "loading"}</p>
         </div>
         <div className="inline-flex text-center items-center mx-auto font-bold w-full">
           {/* ボタンが押されたら状態を書き換える */}
           <div className="my-2 mx-auto border-solid w-1/2 text-center">
             <Card>
               <button
-                className=""
+                className="w-full"
                 onClick={handleDirectionChange}>
                 <p className="">(⇆)左右入替</p></button>
             </Card>
