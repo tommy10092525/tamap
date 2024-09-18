ecbcbb4503c699e3c90ed44f5ba2af1e3831676e
tommy10092525 <tommyxla777@gmail.com>
2024-09-19 06:14:27 +0900

    駅ボタンの修正

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -6,26 +6,29 @@ const Modal = (props: ModalProps) => {
   const { style, handleStationChange } = props;
   return (<div className="flex justify-center text-center w-full scroll-mb-36 mt-3 pb-2">
     {/* ボタンが押されたら状態を書き換える */}
-    <div className='w-2/5 my-auto mx-1 font-bold'>
+    <div className='w-2/5 my-auto mx-1 font-bold'
+      style={style.nishihachioji}>
       <Card>
-        <button className=""
+        <button className="w-full"
           style={style.nishihachioji} onClick={() => {
             handleStationChange("nishihachioji")
           }}><p className="text-sm">西八王子</p></button>
       </Card>
     </div>
-    <div className='w-2/5 my-auto mx-1 font-bold'>
+    <div className='w-2/5 my-auto mx-1 font-bold'
+      style={style.mejirodai}>
       <Card>
-        <button className=""
-          style={style.nishihachioji} onClick={() => {
+        <button className="w-full"
+          style={style.mejirodai} onClick={() => {
             handleStationChange("mejirodai")
           }}><p className="text-sm">めじろ台</p></button>
       </Card>
     </div>
-    <div className='w-2/5 my-auto mx-1 font-bold'>
+    <div className='w-2/5 my-auto mx-1 font-bold'
+      style={style.aihara}>
       <Card>
-        <button className=""
-          style={style.nishihachioji} onClick={() => {
+        <button className="w-full"
+          style={style.aihara} onClick={() => {
             handleStationChange("aihara")
           }}><p className="text-sm">相原</p></button>
       </Card>
