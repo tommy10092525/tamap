8eb5ff1605d9a55bdc218302f02004b6348afa23
tommy10092525 <tommyxla777@gmail.com>
2024-09-19 22:00:27 +0900

    下部のボタンのデザインの修正

--- src/app/components/LinkBox.tsx
+++ src/app/components/LinkBox.tsx
@@ -4,11 +4,14 @@ import Card from './Card'
 // eslint-disable-next-line react/display-name
 const LinkBox = React.memo((props: { children: ReactNode }) => {
     return (
-        <div className="font-bold mb-1 mx-1 w-5/12 text-center">
-            <Card>
-                {props.children}
-            </Card>
+        <div className='w-1/2'>
+            <div className="font-bold mb-1 mx-1 w-auto text-center">
+                <Card>
+                    {props.children}
+                </Card>
+            </div>
         </div>
+
     )
 })
 
