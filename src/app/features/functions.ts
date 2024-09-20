3b81649aa256a95460cc17499e40d6dbd97b941c
tommy10092525 <tommyxla777@gmail.com>
2024-09-21 02:38:38 +0900

    ブランチがおかしくなったので修正

--- src/app/features/functions.ts
+++ src/app/features/functions.ts
@@ -116,12 +116,4 @@ const timeTableFetcher = async (key: string) => {
 }
 
 
-<<<<<<< HEAD
 export { findNextBuses, dayIndices, timeToMinutes, minutesToTime, holidaysFetcher, timeTableFetcher, }
-=======
-<<<<<<< HEAD
-export { findNextBuses, dayIndices, timeToMinutes, minutesToTime, holidaysFetcher, timeTableFetcher, }
-=======
-export { findNextBuses,dayIndices, timeToMinutes, minutesToTime,holidaysFetcher,timeTableFetcher,}
->>>>>>> refs/remotes/origin/main
->>>>>>> refs/remotes/origin/main
