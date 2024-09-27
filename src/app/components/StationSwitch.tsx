1ff5b132098008b0734a51ad6f80bf7c1c59b38f
tommy10092525 <tommyxla777@gmail.com>
2024-09-28 03:09:46 +0900

    画面幅が小さい場合に表示が崩れる問題の修正

--- src/app/components/StationSwitch.tsx
+++ src/app/components/StationSwitch.tsx
@@ -12,7 +12,7 @@ const StationSwitch = React.memo((props: { userInput: UserInput, isLoading: bool
     <div className='my-3 w-full'>
       <Card>
         <div className="inline-flex h-10">
-          <p className="font-bold text-2xl ml-6 mt-[3px]">{isLoading ? "loading" : stationNames[userInput.station]}</p>
+          <p className="font-bold text-2xl ml-2 mt-[3px]">{isLoading ? "loading" : stationNames[userInput.station]}</p>
           {isLoading ? <div></div> : <p className="font-bold text-base mt-3">のバス</p>}
         </div>
         <div className='w-1/2 float-right font-bold mt-2 mr-2 text-center bg-white bg-opacity-50 rounded-md hover:bg-opacity-70'>
