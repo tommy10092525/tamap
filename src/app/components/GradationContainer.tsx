afa96f5803bc85a355b3bd2f25a721f3d65b7bc4
tommy10092525 <tommyxla777@gmail.com>
2024-10-05 01:00:25 +0900

    コンソールでグラデーション内部の状態をチェックしたい

--- src/app/components/GradationContainer.tsx
+++ src/app/components/GradationContainer.tsx
@@ -3,22 +3,23 @@ import React, { ReactNode } from 'react'
 
 // eslint-disable-next-line react/display-name
 const GradationContainer = React.memo((props: { children: ReactNode,hour:number}) => {
-    const { children} = props;
-    const hour=new Date().getMilliseconds()%24
-    console.log(hour);
+    const { children,hour} = props;
     if (8 <= hour && hour < 16) {
+        console.log("昼")
         return (
             <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-emerald-400 to-blue-700 p-5">
                 {children}
             </div>
         )
     } else if (4 <= hour && hour < 20) {
+        console.log("朝/夕")
         return (
             <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-orange-500 to-purple-700 p-5">
                 {children}
             </div>
         )
     } else {
+        console.log("夜")
         return (
             <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-violet-700 to-blue-800 p-5">
                 {children}
