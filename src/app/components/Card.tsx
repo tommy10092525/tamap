ef34c7009d38ed7693c58bfc02885eb9463cc8cf
tommy10092525 <tommyxla777@gmail.com>
2024-10-05 00:22:34 +0900

    不要な記述の削除

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -2,11 +2,10 @@ import React, { ReactNode } from 'react'
 import { Style } from './Types'
 
 // eslint-disable-next-line react/display-name
-const Card = React.memo((props:{children:ReactNode,style?:Style}) => {
-    let {children,style}=props;
+const Card = React.memo((props: { children: ReactNode }) => {
+    let { children } = props;
     return (
-        <div className='shadow rounded-md bg-white dark:bg-gray-200 bg-opacity-50 dark:bg-opacity-50'
-            style={style}>
+        <div className='shadow rounded-md bg-white dark:bg-gray-200 bg-opacity-50 dark:bg-opacity-50'>
             {children}
         </div>
     )
