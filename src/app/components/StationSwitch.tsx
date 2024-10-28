c4ca79871b5347a488ab85634ec1666368e813fa
tommy10092525 <tommyxla777@gmail.com>
2024-10-28 12:43:51 +0900

    駅名を横並びにした

--- src/app/components/StationSwitch.tsx
+++ src/app/components/StationSwitch.tsx
@@ -4,12 +4,13 @@ import Modal from './Modal';
 import { StationNames } from './Types';
 import Card from './Card';
 import { Button } from '@chakra-ui/react';
-import { MenuRoot,MenuTrigger,MenuContent,MenuItem } from '@chakra-ui/react';
+import { MenuRoot, MenuTrigger, MenuContent, MenuItem } from '@chakra-ui/react';
+import { stationNames } from '@/constants/settings';
 
-const stationNames: StationNames = { nishihachioji: "西八王子", mejirodai: "めじろ台", aihara: "相原" };
+
 
 // eslint-disable-next-line react/display-name
-const StationSwitch =(props: { userInput: UserInput, isLoading: boolean, handleShowModalChange: () => void, handleStationChange: (station: string) => void }) => {
+const StationSwitch = (props: { userInput: UserInput, isLoading: boolean, handleShowModalChange: () => void, handleStationChange: (station: string) => void }) => {
   let { userInput, isLoading, handleShowModalChange, handleStationChange } = props;
   return (
     <div className='my-3 w-full'>
@@ -18,28 +19,26 @@ const StationSwitch =(props: { userInput: UserInput, isLoading: boolean, handleS
           <p className="font-bold text-2xl ml-2 mt-3">{isLoading ? "loading" : stationNames[userInput.station]}</p>
           {isLoading ? <div></div> : <p className="font-bold text-base mt-5">のバス</p>}
         </div>
-        {/* <div className='w-1/2 float-right font-bold mt-2 mr-2 text-center bg-white bg-opacity-50 rounded-md hover:bg-opacity-70 shadow'>
-          <Button className="w-full h-2"
-            onClick={handleShowModalChange}>
-            バスを変更
-          </Button></div>
-        {userInput.showModal ? <Modal handleStationChange={handleStationChange} userInput={userInput} /> : <></>} */}
         <MenuRoot>
           <MenuTrigger
             asChild
             className='bg-white bg-opacity-50 float-right'>
-            <Button className='bg-white bg-opacity-50 rounded-md shadow m-2 p-2 font-bold hover:bg-opacity-70'>バスを変更</Button>
+            <Button className='bg-white bg-opacity-50 rounded-md shadow m-2 p-2 font-bold hover:bg-blue-500 transition-colors'>バスを変更</Button>
           </MenuTrigger>
           <MenuContent className='bg-white bg-opacity-0 border-0 shadow-none'>
-            <MenuItem value="nishihachioji" onClick={()=>{
-              handleStationChange("nishihachioji");
-            }} className='bg-white bg-opacity-50 shadow rounded-md hover:bg-blue-300 font-bold'>西八王子</MenuItem>
-            <MenuItem value="mejirodai" onClick={()=>{
-              handleStationChange("mejirodai");
-            }} className='bg-white bg-opacity-50 shadow rounded-md my-1 hover:bg-blue-300 font-bold'>めじろ台</MenuItem>
-            <MenuItem value="aihara" onClick={()=>{
-              handleStationChange("aihara");
-            }} className='bg-white bg-opacity-50 shadow rounded-md hover:bg-blue-300 font-bold'>相原</MenuItem>
+            {Object.entries(stationNames).map(([key, value]) => {
+              return (
+                <MenuItem value={key} key={key}
+                  className='bg-white bg-opacity-0 m-0 px-2 py-0 w-1/3 float-left'>
+                  <Button
+                  size={"xs"}
+                  className='bg-white bg-opacity-50 shadow rounded-md hover:bg-blue-500 font-bold my-1 transition-colors float-left px-1 w-full'
+                  onClick={() => {
+                    handleStationChange(key);
+                  }}>{value}</Button>
+                </MenuItem>
+              )
+            })}
           </MenuContent>
         </MenuRoot>
       </Card>
