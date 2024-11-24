186756865756e3fd81b792efcd0add404a2e0d32
tommy10092525 <tommyxla777@gmail.com>
2024-11-24 17:01:42 +0900

    stateの変化を関数で記述

--- src/app/features/timeHandlers.ts
+++ src/app/features/timeHandlers.ts
@@ -5,121 +5,121 @@ const dayIndices = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Fri
 
 // 時刻を数値に変換するヘルパー関数（分単位）
 function toMinutes(hour: number, minutes: number) {
-    return hour * 60 + minutes;
+  return hour * 60 + minutes;
 }
 
 // 指定された時刻（分）との差を計算
 function timeDifference(nowInMinutes: number, busInMinutes: number) {
-    return busInMinutes - nowInMinutes;
+  return busInMinutes - nowInMinutes;
 }
 
 // 日付が祝日かどうかを判定
 function isHoliday(date: Date, holidayData: Holidays) {
-    date.setDate(date.getDate()+timeOfEquation);
-    const formattedDate = date.toISOString().split('T')[0];
-    if (!holidayData) {
-        alert("祝日データがpending状態です！！！");
-        return false;
-    }
-    return holidayData.hasOwnProperty(formattedDate);
+  date.setDate(date.getDate() + timeOfEquation);
+  const formattedDate = date.toISOString().split('T')[0];
+  if (!holidayData) {
+    alert("祝日データがpending状態です！！！");
+    return false;
+  }
+  return holidayData.hasOwnProperty(formattedDate);
 }
 
 // 平日かどうかを判定
 function isWeekday(day: string) {
-    return ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"].includes(day);
+  return ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"].includes(day);
 }
 
 // 次の曜日を取得する関数（祝日も「日曜日」として扱う）
 function getNextDay(currentDay: string, currentDate: Date, holidayData: Holidays) {
-    let nextDate = new Date(currentDate);
-    nextDate.setDate(nextDate.getDate() + 1);
-    if (isHoliday(nextDate, holidayData)) {
-        return "Sunday"; // 祝日を日曜日と扱う
-    }
-    const nextDayIndex = (dayIndices.indexOf(currentDay) + 1) % 7;
-    return dayIndices[nextDayIndex];
+  let nextDate = new Date(currentDate);
+  nextDate.setDate(nextDate.getDate() + 1);
+  if (isHoliday(nextDate, holidayData)) {
+    return "Sunday"; // 祝日を日曜日と扱う
+  }
+  const nextDayIndex = (dayIndices.indexOf(currentDay) + 1) % 7;
+  return dayIndices[nextDayIndex];
 }
 
 // 祝日も日曜日とする
 function getPreviousDay(currentDay: string, currentDate: Date, holidayData: Holidays) {
-    let previousDate = new Date(currentDate);
-    previousDate.setDate(previousDate.getDate() - 1);
-    if (isHoliday(previousDate, holidayData)) {
-        return "Sunday";
-    }
-    const previousDayIndex = mod((dayIndices.indexOf(currentDay) - 1), 7);
-    return dayIndices[previousDayIndex];
+  let previousDate = new Date(currentDate);
+  previousDate.setDate(previousDate.getDate() - 1);
+  if (isHoliday(previousDate, holidayData)) {
+    return "Sunday";
+  }
+  const previousDayIndex = mod((dayIndices.indexOf(currentDay) - 1), 7);
+  return dayIndices[previousDayIndex];
 }
 
 // 次のバスを検索
 function findBuses(args: {
-    timeTable: TimeTable, holidayData: Holidays, busesLength: number,
-    currentDay: string, currentHour: number, currentMinutes: number, currentDate: Date
+  timeTable: TimeTable, holidayData: Holidays, busesLength: number,
+  currentDay: string, currentHour: number, currentMinutes: number, currentDate: Date
 }) {
 
-    const { holidayData, currentDay, currentHour, currentMinutes, currentDate, busesLength } = args;
-    let { timeTable } = args;
-    const nowInMinutes = toMinutes(currentHour, currentMinutes);
-    let findedBuses = [];
-    // 現在の曜日のバスを取得
-    let dayToCheck: string;
-    if (isHoliday(currentDate, holidayData)) {
-        dayToCheck = "Sunday"
-    } else {
-        dayToCheck = currentDay;
-    }
-    let dateToCheck = currentDate;
-    // 以前のバスを取得する場合には時刻表を前後逆にする
-    if (busesLength < 0) {
-        timeTable.reverse();
-    }
-    // バスが見つかるまで次の日に進む（曜日を変えていく）  
-    for (let i = 0; i < 7; i++) {
-        const busesForDay = timeTable.filter(bus =>
-            bus.day === dayToCheck || (isWeekday(dayToCheck) && bus.day === "weekday")
-        );
-        for (let bus of busesForDay) {
-            const busLeaveTime = toMinutes(bus.leaveHour, bus.leaveMinute);
+  const { holidayData, currentDay, currentHour, currentMinutes, currentDate, busesLength } = args;
+  let { timeTable } = args;
+  const nowInMinutes = toMinutes(currentHour, currentMinutes);
+  let findedBuses = [];
+  // 現在の曜日のバスを取得
+  let dayToCheck: string;
+  if (isHoliday(currentDate, holidayData)) {
+    dayToCheck = "Sunday"
+  } else {
+    dayToCheck = currentDay;
+  }
+  let dateToCheck = currentDate;
+  // 以前のバスを取得する場合には時刻表を前後逆にする
+  if (busesLength < 0) {
+    timeTable.reverse();
+  }
+  // バスが見つかるまで次の日に進む（曜日を変えていく）  
+  for (let i = 0; i < 7; i++) {
+    const busesForDay = timeTable.filter(bus =>
+      bus.day === dayToCheck || (isWeekday(dayToCheck) && bus.day === "weekday")
+    );
+    for (let bus of busesForDay) {
+      const busLeaveTime = toMinutes(bus.leaveHour, bus.leaveMinute);
 
-            // 現在の曜 日かつ未来/過去のバス、または翌日/前日のバス
-            if (busesLength > 0) {
-                if (i > 0 || timeDifference(nowInMinutes, busLeaveTime) >= 0) {
-                    findedBuses.push(bus);
-                }
-            } else if (busesLength < 0) {
-                if (i > 0 || timeDifference(nowInMinutes, busLeaveTime) < 0) {
-                    findedBuses.push(bus)
-                }
-            }
-            else {
-                return [];
-            }
-            if (findedBuses.length >= Math.abs(busesLength)) {
-                return findedBuses;
-            }
+      // 現在の曜 日かつ未来/過去のバス、または翌日/前日のバス
+      if (busesLength > 0) {
+        if (i > 0 || timeDifference(nowInMinutes, busLeaveTime) >= 0) {
+          findedBuses.push(bus);
         }
-        // 次の日に進む
-        dayToCheck = (busesLength > 0 ? getNextDay(dayToCheck, dateToCheck, holidayData) : getPreviousDay(dayToCheck, dateToCheck, holidayData));
-        dateToCheck.setDate(dateToCheck.getDate() + (busesLength > 0 ? 1 : -1));
+      } else if (busesLength < 0) {
+        if (i > 0 || timeDifference(nowInMinutes, busLeaveTime) < 0) {
+          findedBuses.push(bus)
+        }
+      }
+      else {
+        return [];
+      }
+      if (findedBuses.length >= Math.abs(busesLength)) {
+        return findedBuses;
+      }
     }
-    return findedBuses;
+    // 次の日に進む
+    dayToCheck = (busesLength > 0 ? getNextDay(dayToCheck, dateToCheck, holidayData) : getPreviousDay(dayToCheck, dateToCheck, holidayData));
+    dateToCheck.setDate(dateToCheck.getDate() + (busesLength > 0 ? 1 : -1));
+  }
+  return findedBuses;
 }
 
 // `hh:mm` を分単位に変換する関数
-const timeToMinutes = (time: string) => {
-    const [hours, minutes] = time.split(":").map(Number);
-    return hours * 60 + minutes;
+function timeToMinutes(time: string) {
+  const [hours, minutes] = time.split(":").map(Number);
+  return hours * 60 + minutes;
 };
 
 // 分単位を `hh:mm` に戻す関数
-const minutesToTime = (minutes: number) => {
-    const hours = String(Math.floor(minutes / 60));
-    const mins = String(minutes % 60).padStart(2, "0");
-    return `${hours}:${mins}`;
+function minutesToTime(minutes: number) {
+  const hours = String(Math.floor(minutes / 60));
+  const mins = String(minutes % 60).padStart(2, "0");
+  return `${hours}:${mins}`;
 };
 
 const mod = (a: number, b: number) => {
-    return ((a % b) + b) % b;
+  return ((a % b) + b) % b;
 }
 
 export { findBuses, dayIndices, timeToMinutes, minutesToTime, isHoliday }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,7 @@ import { initializeCaption, holidaysFetcher, timeTableFetcher } from "./features
 import GradationContainer from "./components/GradationContainer";
 import Link from "next/link";
 import { Button } from "@chakra-ui/react";
+// import { Button, Label, ListBox, ListBoxItem, Popover, Select, SelectValue } from 'react-aria-components';
 
 
 // 現在の時刻と曜日を取得
@@ -39,7 +40,11 @@ const Home = () => {
       let isComingToHosei: string | null = localStorage.getItem("isComingToHosei");
       let station: string | null = localStorage.getItem("station");
       if ("string" === typeof isComingToHosei && "string" === typeof station) {
-        setUserInput({ isComingToHosei: true, station: station, showModal: false });
+        setUserInput(prev => {
+          prev.isComingToHosei = isComingToHosei === "true";
+          prev.station = station;
+          return prev;
+        });
       } else {
         alert("localStorageのエラー");
       }
@@ -50,13 +55,14 @@ const Home = () => {
       localStorage.setItem("station", "nishihachioji");
     }
   }
+  console.log(userInput);
 
   // ページ読み込み時の処理
   // https://qiita.com/iwakeniwaken/items/3c3e212599e411da54e2
   useEffect(() => {
     rerender([]);
     const interval = setInterval(() => {
-      rerender([]);
+      rerender(prev => []);
     }, 1000);
     initializeUserInput();
     return () => clearInterval(interval);
@@ -69,54 +75,78 @@ const Home = () => {
   if (!isTimeTableLoading && !!timeTable && !isHolidayLoading && !!holidayData) {
     // 駅と方向から絞る
     timeTable = timeTable
-      .filter(item => item.isComingToHosei == userInput.isComingToHosei && item.station == userInput.station)
-    futureBuses = findBuses({ timeTable, holidayData, currentDay, currentHour, currentMinutes, currentDate: now, busesLength: 3 });
-    previousBuses = findBuses({ timeTable, holidayData, currentDay, currentHour, currentMinutes, currentDate: now, busesLength: -3 });
+      .filter(item => item.isComingToHosei == userInput.isComingToHosei && item.station == userInput.station);
+    futureBuses = findBuses({
+      timeTable,
+      holidayData,
+      currentDay,
+      currentHour,
+      currentMinutes,
+      currentDate: now,
+      busesLength: 3
+    });
+    previousBuses = findBuses({
+      timeTable,
+      holidayData,
+      currentDay,
+      currentHour,
+      currentMinutes,
+      currentDate: now,
+      busesLength: -3
+    });
 
 
   } else {
-    futureBuses = []
-    previousBuses = []
+    futureBuses = [];
+    previousBuses = [];
   }
-  caption = useMemo(() => initializeCaption({ userInput, minutesToTime, futureBuses, previousBuses, isLoading: isHolidayLoading || isTimeTableLoading }),
-    [futureBuses, previousBuses, userInput, isHolidayLoading, isTimeTableLoading]);
+  caption = useMemo(() => initializeCaption({
+    userInput,
+    minutesToTime,
+    futureBuses,
+    previousBuses,
+    isLoading: isHolidayLoading || isTimeTableLoading
+  }), [futureBuses, previousBuses, userInput, isHolidayLoading, isTimeTableLoading]);
   // API取得にエラーが生じた場合エラーをコンソールに吐く
   if (timeTableError) {
     console.log(timeTableError);
   }
 
-  const handleDirectionChange = useCallback(() => {
-    if (userInput.isComingToHosei) {
-      let nextUserInput = structuredClone(userInput);
-      nextUserInput.isComingToHosei = false;
-      setUserInput(nextUserInput);
-      localStorage.setItem("isComingToHosei", "false")
-    } else {
-      let nextUserInput = structuredClone(userInput);
-      nextUserInput.isComingToHosei = true;
-      setUserInput(nextUserInput);
-      localStorage.setItem("isComingToHosei", "true")
-    }
-  }, [userInput])
+  const handleDirectionChange = () => {
+    setUserInput(prev => {
+      let next = structuredClone(prev);
+      if (next.isComingToHosei) {
+        next.isComingToHosei = false;
+        localStorage.setItem("isComingToHosei", "false")
+      } else {
+        next.isComingToHosei = true;
+        localStorage.setItem("isComingToHosei", "true")
+      }
+      return next;
+    })
+  }
 
-  const handleShowModalChange = useCallback(() => {
-    let nextUserInput = structuredClone(userInput);
-    nextUserInput.showModal = !nextUserInput.showModal;
-    setUserInput(nextUserInput);
-  }, [userInput])
+  const handleShowModalChange = () => {
+    setUserInput(prev => {
+      let next = structuredClone(prev);
+      next.showModal = !next.showModal;
+      return next;
+    })
+  }
 
-  const handleStationChange = useCallback((station: string) => {
-    let nextUserInput = structuredClone(userInput);
-    nextUserInput.station = station;
-    nextUserInput.showModal = false;
-    setUserInput(nextUserInput);
+  const handleStationChange = (station: string) => {
+    setUserInput(prev => {
+      let next = structuredClone(prev);
+      next.station = station;
+      next.showModal = false;
+      return next;
+    })
     localStorage.setItem("station", station);
-  }, [userInput])
+  }
 
 
   return (
-    <div
-      className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-bl from-sky-500 to-orange-300 p-5 dark:text-black">
+    <div className="flex flex-col justify-center items-center bg-gradient-to-bl from-sky-500 to-orange-300 p-5 min-h-screen dark:text-black">
       <div className="max-w-screen-sm">
         <Logo />
         <TimeCaption
@@ -137,26 +167,13 @@ const Home = () => {
         <DiscountInformation text="飲食店割引はこちら" />
 
         <div className="flex flex-wrap justify-center w-full">
-          {/* <LinkBox><Link href={GoogleForm}>アプリご意見</Link></LinkBox>
-          <LinkBox><Button onClick={async () => {
-            try {
-              await navigator.share({
-                title: "たまっぷ!(Next.js)",
-                text: codematesHP.concat("tamap/")
-              });
-            } catch (error) {
-              console.log("Error sharing:", error);
-            }
-          }}>アプリを共有</Button></LinkBox>
-          <LinkBox><Link href={codematesHP}>CODE MATESとは</Link></LinkBox>
-          <LinkBox><Link href={Instagram}>Instagram</Link></LinkBox> */}
-          <div className="w-1/2 px-1">
-            <Button className="w-full font-bold mb-1 mx-1 text-center bg-white bg-opacity-50 rounded-md shadow float-left" size="xs">
+          <div className="px-1 w-1/2">
+            <Button className="float-left bg-white bg-opacity-50 shadow mx-1 mb-1 rounded-md w-full font-bold text-center" size="xs">
               <Link href={GoogleForm} className="w-full">アプリご意見</Link>
             </Button>
           </div>
-          <div className="w-1/2 px-1">
-            <Button className="w-full font-bold mb-1 mx-1 text-center bg-white bg-opacity-50 rounded-md shadow float-left" size="xs" onClick={async()=>{
+          <div className="px-1 w-1/2">
+            <Button className="float-left bg-white bg-opacity-50 shadow mx-1 mb-1 rounded-md w-full font-bold text-center" size="xs" onClick={async () => {
               try {
                 await navigator.share({
                   title: "たまっぷ!(Next.js)",
@@ -167,18 +184,18 @@ const Home = () => {
               }
             }}>アプリを共有</Button>
           </div>
-          <div className="w-1/2 px-1">
-            <Button className="w-full font-bold mb-1 mx-1 text-center bg-white bg-opacity-50 rounded-md shadow float-left" size="xs">
+          <div className="px-1 w-1/2">
+            <Button className="float-left bg-white bg-opacity-50 shadow mx-1 mb-1 rounded-md w-full font-bold text-center" size="xs">
               <Link href={codematesHP} className="w-full">CODE MATESとは</Link>
             </Button>
           </div>
-          <div className="w-1/2 px-1">
-            <Button className="w-full font-bold mb-1 mx-1 text-center bg-white bg-opacity-50 rounded-md shadow float-left" size="xs">
+          <div className="px-1 w-1/2">
+            <Button className="float-left bg-white bg-opacity-50 shadow mx-1 mb-1 rounded-md w-full font-bold text-center" size="xs">
               <Link href={Instagram} className="w-full">Instagram</Link>
             </Button>
           </div>
         </div>
-        <p className="text-xs text-center">時刻は目安であり、交通状況等によって変わる可能性があります。また臨時便等には対応しておりません。</p>
+        <p className="text-center text-xs">時刻は目安であり、交通状況等によって変わる可能性があります。また臨時便等には対応しておりません。</p>
         <p className="flex justify-center items-center text-center text-lg">©CODE MATES︎</p>
       </div>
     </div>
